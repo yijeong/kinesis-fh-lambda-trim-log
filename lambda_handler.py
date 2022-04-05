3de7d206a0e1c7a58a8cf077c38ee8f8bf504a73
이정 조 <yijeong.cho@ijeong-ui-MacBookPro.local>
2022-04-05 13:33:54 +0900

    update code

--- lambda_handler.py
+++ lambda_handler.py
@@ -10,10 +10,16 @@ def lambda_handler(event, context):
         print(record['recordId'])
         payload = base64.b64decode(record['data']).decode('utf-8')
         payload_json = json.loads(payload)
-
+        
         if "firewall_rule_action" not in payload_json: 
             print ("Trimmed")
-            pass 
+            output_record = {
+                'recordId': record['recordId'],
+                'result': 'Dropped',
+                'data': base64.b64encode(payload.encode('utf-8'))
+            }
+            output.append(output_record)
+
         else : 
             output_record = {
                 'recordId': record['recordId'],
@@ -22,6 +28,5 @@ def lambda_handler(event, context):
             }
             output.append(output_record)
     print('Successfully processed {} records.'.format(len(event['records']))) 
-    return {'records': output}     
-
+    return {'records': output}  
 
